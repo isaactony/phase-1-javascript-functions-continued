8d8fe422a6712e873235aa33961a149a47b68779
isaac <isaactonyloi@gmail.com>
2022-11-27 17:19:49 +0300

    functions-extended

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // code your solution here
+
+let mondayWork = function(task="go to the office") {
+    return `This Monday, I will ${task}.`;
+
+}
+
+function saturdayFun(rollers = "roller-skate") {
+ 
+    const innerFunc = function() {
+        return `This Saturday, I want to ${rollers}!`;
+    };
+    return innerFunc();
+}
+
+
+function wrapAdjective(flair = "*") {
+    return function(message = "special") {
+        return console.log(`You are ${flair}${message}${flair}!`);
+
+    };
+
+}
+//wrapAdjective("||")("a dedicated programmer");
